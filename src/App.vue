eda2a023ab323c5b9551ccaaf5aabf6fae1afc66
Kiti0025 <ts66666620212021@163.com>
2025-09-29 14:30:35 +0800

    删除多余代码

--- src/App.vue
+++ src/App.vue
@@ -16,19 +16,5 @@ export default {
 </script>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 
-body {
-  font-family: 'Microsoft YaHei', 'PingFang SC', 'Helvetica Neue', Arial, sans-serif;
-  background-color: #f5f5f5;
-}
-
-#app {
-  width: 100vw;
-  height: 100vh;
-}
 </style>
